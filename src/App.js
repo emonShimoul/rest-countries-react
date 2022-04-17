278f8ef912b4de1d082df17671a18765c36aafa4
emonShimoul <emonshimoul@gmail.com>
2022-04-17 10:01:59 +0600

    remove unusual comments

--- src/App.js
+++ src/App.js
@@ -11,34 +11,4 @@ function App() {
     </div>
   );
 }
-
-// function Countries() {
-//   const [countries, setCountries] = useState([]);
-//   console.log(countries);
-//   useEffect(()=>{
-//     fetch('https://restcountries.com/v3.1/all')
-//     .then(res => res.json())
-//     .then(data => setCountries(data))
-//   },[]);
-
-//   return (
-//     <div>
-//       <h1>Traveling around the world!!</h1>
-//       <h4>Countries Available: {countries.length}</h4>
-//       {
-//         countries.map(country => <Country name={country.name.common} capital={country.capital}></Country>)
-//       }
-//     </div>
-//   )
-// }
-
-// function Country(props) {
-//   return (
-//     <div>
-//       <h2>Name: {props.name}</h2>
-//       <p>Capital: {props.capital}</p>
-//     </div>
-//   );
-// }
-
 export default App;
